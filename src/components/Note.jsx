601bd52b08d151a83072c42c1df92921f867e70e
RyanKaleliGabriel <ryang9241@gmail.com>
2023-06-16 10:50:10 +0300

    Components

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note(){
+    return(
+    <div className="note">
+        <h1 className="">This is the note Heading.</h1>
+        <p className="">This is the note content.</p>
+    </div>);
+}
+
+export default Note;
